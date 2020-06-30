579141c20ad9964c385eaf31be3caebc1e0e3e2c
Alex Rothenberg <alexroth96@gmail.com>
2020-06-29 20:46:15 -0400

    add in prod api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const devUrl = 'http://localhost:5000/api';
-const url = `https://crash-the-calm.herokuapp.com/`;
+const url = `https://crash-the-calm.herokuapp.com/api`;
 const api = axios.create({
 	baseURL: url,
 	headers: {
