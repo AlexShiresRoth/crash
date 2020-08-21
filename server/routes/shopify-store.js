4789b9f1b5c067f49204a6501c9cfa6d0555e2de
Alex Rothenberg <alexroth96@gmail.com>
2020-08-20 22:36:27 -0400

    get shopify connected

--- server/routes/shopify-store.js
+++ server/routes/shopify-store.js
@@ -1,17 +1,39 @@
 const express = require('express');
 require('es6-promise').polyfill();
 require('isomorphic-fetch');
+const Client = require('shopify-buy');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	fetch('https://ctc-test-app.myshopify.com/admin/api/graphql.json', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Shopify-Access-Token': process.env.SHOPIFY_KEY,
-		},
-		body: JSON.stringify({
-			query: `{
+const client = Client.buildClient({
+	domain: 'crash-the-calm.myshopify.com',
+	storefrontAccessToken: 'b8d283306bab2fd1c7009809c880b300',
+});
+
+router.get('/orders', async (req, res) => {
+	try {
+		const response = await client.product.fetchAll();
+
+		if (!response) {
+			return res.status(400).json({ msg: 'Oops no response!' });
+		}
+		console.log(response);
+		res.json(response);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ msg: 'Internal Server Error' });
+	}
+});
+
+router.get('/', async (req, res) => {
+	try {
+		const response = await fetch('https://crash-the-calm.myshopify.com/admin/api/2020-07/orders.json', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'X-Shopify-Access-Token': process.env.SHOPIFY_SECRET,
+			},
+			body: JSON.stringify({
+				query: `{
          shop {
            name
            url
@@ -19,16 +41,14 @@ router.get('/', (req, res) => {
            myshopifyDomain
          }
        }`,
-		}),
-	})
-		.then((result) => {
-			console.log(result);
-			return result.json();
-		})
-		.then((data) => {
-			console.log('data returned:\n', data);
-			res.send(data);
+			}),
 		});
+		console.log(response);
+		res.json(response);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ msg: 'Internal Server Error' });
+	}
 });
 
 module.exports = router;
