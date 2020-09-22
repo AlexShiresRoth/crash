ca0077ab94714268bc52f82d8e0aff1bd3a941c1
Alex Rothenberg <alexroth96@gmail.com>
2020-09-21 20:35:11 -0400

    add new hero image for merch

--- src/components/main/StoreSection.tsx
+++ src/components/main/StoreSection.tsx
@@ -14,20 +14,7 @@ const StoreSection = () => {
 	return (
 		<section className={style.box}>
 			<Link to={section.path}>Shop Merch</Link>
-			<div className={style.container}>
-				<img
-					src={`https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_64,w_966/v1593471982/crash/ctc-longsleeveheart-mockup_agc99r.png`}
-					alt="merch"
-				/>
-				<img
-					src={`https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_65,w_1413/v1593472008/crash/CD-mockup-nobg_jmyojh.png`}
-					alt="merch"
-				/>
-				<img
-					src={`https://res.cloudinary.com/snackmanproductions/image/upload/c_scale,q_67,w_1036/v1593473180/crash/ctc-pullover-mockups_co3w0z.png`}
-					alt="merch"
-				/>
-			</div>
+			<div className={style.container}></div>
 		</section>
 	);
 };
