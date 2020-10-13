8bc8275f303117c44f14b276524001db6838363d
Alex Rothenberg <alexroth96@gmail.com>
2020-10-12 21:06:09 -0400

    update single page

--- src/components/single/SingleComponent.tsx
+++ src/components/single/SingleComponent.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import ReactPlayer from 'react-player/lazy';
+import { Link } from 'react-router-dom';
 import style from './SingleComponent.module.scss';
 
 const SingleComponent = (props: any) => {
 	return (
 		<section className={style.section}>
 			<div className={style.video_container}>
-				<ReactPlayer url="https://youtu.be/xrJNa0Z5Rp8" width={`100%`} height={`100%`} />
+				<h2>Watch The "Devils" Single Lyric video</h2>
+				<ReactPlayer url="https://youtu.be/xrJNa0Z5Rp8" width={`100%`} height={`100%`} allowFullscreen={true} />
 			</div>
 			<div className={style.img_container}>
-				<img
-					src="https://res.cloudinary.com/snackmanproductions/image/upload/v1602463223/crash/devilsartwork-notext_f3v5vd.jpg"
-					alt="devils merch"
-				/>
+				<Link to="/store">Shop "Devils" Merch</Link>
+				<div className={style.bg_img}>
+					<Link to="/store"></Link>
+				</div>
 			</div>
 		</section>
 	);
