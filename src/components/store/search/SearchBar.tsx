c43557110beb33306db31b24a416db0edd063e17
Alex Rothenberg <alexroth96@gmail.com>
2021-04-19 09:12:21 -0400

    add more crumbs

--- src/components/store/search/SearchBar.tsx
+++ src/components/store/search/SearchBar.tsx
@@ -3,6 +3,7 @@ import style from './SearchBar.module.scss';
 import { FaSearch } from 'react-icons/fa';
 import { searchCatalog, clearSearch } from '../../../actions/store';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 interface Props {
 	searchCatalog: (data: any) => any;
@@ -40,35 +41,44 @@ const SearchBar = ({ searchCatalog, clearSearch, store: { catalog, loading } }: 
 
 	return (
 		<div className={style.search}>
-			<div className={style.form_container}>
-				<form onSubmit={(e) => formSubmit(e)}>
-					{!loading && catalog ? (
-						<select onChange={(e) => onSelectChange(e)} name="searchTerm">
-							{catalog.map((item: any, i: number) => {
-								return (
-									<option value={item.productType} key={i}>
-										{item.productType}
-									</option>
-								);
-							})}
-						</select>
-					) : (
-						<p>Loading Categories...</p>
-					)}
+			<div className={style.content}>
+				<div className={style.bread_crumbs}>
+					<Link to={`/`}>Home</Link>
+					<p>\</p>
+					<Link to={'/store'} className={style.active}>
+						Store
+					</Link>
+				</div>
+				<div className={style.form_container}>
+					<form onSubmit={(e) => formSubmit(e)}>
+						{!loading && catalog ? (
+							<select onChange={(e) => onSelectChange(e)} name="searchTerm">
+								{catalog.map((item: any, i: number) => {
+									return (
+										<option value={item.productType} key={i}>
+											{item.productType}
+										</option>
+									);
+								})}
+							</select>
+						) : (
+							<p>Loading Categories...</p>
+						)}
 
-					<input
-						name="searchTerm"
-						value={searchTerm}
-						onChange={(e) => onChange(e)}
-						placeholder="Search store catalog"
-					></input>
-					<button onSubmit={(e) => formSubmit(e)}>
-						<FaSearch />
-					</button>
-				</form>
+						<input
+							name="searchTerm"
+							value={searchTerm}
+							onChange={(e) => onChange(e)}
+							placeholder="Search store catalog"
+						></input>
+						<button onSubmit={(e) => formSubmit(e)}>
+							<FaSearch />
+						</button>
+					</form>
 
-				<div className={style.clear_btn}>
-					<button onClick={(e) => handleClearSearch()}>Clear Search</button>
+					<div className={style.clear_btn}>
+						<button onClick={(e) => handleClearSearch()}>Clear Search</button>
+					</div>
 				</div>
 			</div>
 		</div>
