dda6154054935619829566b7400a4d9bd0ec71c7
Alex <alexroth96@gmail.com>
2021-07-08 14:48:04 -0400

    fixing things

--- components/store/checkout/ShippingAddressForm.tsx
+++ components/store/checkout/ShippingAddressForm.tsx
@@ -1,166 +1,182 @@
-import React, { useState, useEffect } from 'react';
-import style from './ShippingAddressForm.module.scss';
-import { connect } from 'react-redux';
-import { submitShippingInfo } from '../../../actions/store';
-import StoreAlert from '../alerts/StoreAlert';
-import { states } from '../../reusablecomps/states';
-import LoadingSpinner from '../../reusablecomps/LoadingSpinner';
+import React, { useState, useEffect } from "react";
+import style from "./ShippingAddressForm.module.scss";
+import { connect } from "react-redux";
+import { submitShippingInfo } from "../../../redux/actions/store";
+import StoreAlert from "../alerts/StoreAlert";
+import { states } from "../../reusablecomps/states";
+import LoadingSpinner from "../../reusablecomps/LoadingSpinner";
 
 interface Props {
-	submitShippingInfo: (data: any, val: boolean) => any;
-	store?: any;
-	alerts: Array<any>;
+  submitShippingInfo: (data: any, val: boolean) => any;
+  store?: any;
+  alerts: Array<any>;
 }
 
-const ShippingAddressForm = ({ submitShippingInfo, store: { checkout, shippingErrors }, alerts }: Props) => {
-	const [formData, setFormData] = useState<any>({
-		address1: '',
-		address2: '',
-		city: '',
-		company: null,
-		firstName: '',
-		lastName: '',
-		phone: '',
-		province: '',
-		zip: '',
-		checkoutId: '',
-	});
+const ShippingAddressForm = ({
+  submitShippingInfo,
+  store: { checkout, shippingErrors },
+  alerts,
+}: Props) => {
+  const [formData, setFormData] = useState<any>({
+    address1: "",
+    address2: "",
+    city: "",
+    company: null,
+    firstName: "",
+    lastName: "",
+    phone: "",
+    province: "",
+    zip: "",
+    checkoutId: "",
+  });
 
-	const { address1, zip, firstName, lastName, city, province } = formData;
+  const { address1, zip, firstName, lastName, city, province } = formData;
 
-	const onChange = (e: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLSelectElement>) =>
-		setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value });
+  const onChange = (
+    e: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLSelectElement>
+  ) =>
+    setFormData({ ...formData, [e.currentTarget.name]: e.currentTarget.value });
 
-	const formSubmit = (e: React.FormEvent) => {
-		e.preventDefault();
-		setProcessing(true);
-		submitShippingInfo(formData, false);
-	};
+  const formSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setProcessing(true);
+    submitShippingInfo(formData, false);
+  };
 
-	useEffect(() => {
-		if (localStorage.getItem('checkout')) {
-			setFormData(() => ({
-				address1,
-				address2: '',
-				city,
-				company: null,
-				firstName,
-				lastName,
-				phone: '',
-				province,
-				zip,
-				checkoutId: localStorage.getItem('checkout'),
-			}));
-		}
-	}, [address1, zip, firstName, lastName, city, province, checkout]);
+  useEffect(() => {
+    if (localStorage.getItem("checkout")) {
+      setFormData(() => ({
+        address1,
+        address2: "",
+        city,
+        company: null,
+        firstName,
+        lastName,
+        phone: "",
+        province,
+        zip,
+        checkoutId: localStorage.getItem("checkout"),
+      }));
+    }
+  }, [address1, zip, firstName, lastName, city, province, checkout]);
 
-	const [processing, setProcessing] = useState<boolean>(false);
+  const [processing, setProcessing] = useState<boolean>(false);
 
-	useEffect(() => {
-		if (shippingErrors.length > 0) setProcessing(false);
-	}, [shippingErrors]);
+  useEffect(() => {
+    if (shippingErrors.length > 0) setProcessing(false);
+  }, [shippingErrors]);
 
-	return (
-		<div className={style.form_container}>
-			<div className={style.form_header}>
-				<h2>Shipping Address Form</h2>
-				<p>Currently, only shipping within the US</p>
-				{shippingErrors.length > 0
-					? alerts.map((alert, i) => <StoreAlert status={alert.msg} key={i} type={alert.alertType} />)
-					: null}
-			</div>
-			<form className={style.form} onSubmit={(e) => formSubmit(e)}>
-				<div className={style.grid}>
-					<div className={style.col}>
-						<div className={style.input_col}>
-							<label>First Name</label>
-							<input
-								type="text"
-								value={firstName}
-								name="firstName"
-								placeholder="Enter your name"
-								onChange={(e) => onChange(e)}
-								required={true}
-							/>
-						</div>
-						<div className={style.input_col}>
-							<label>Last Name</label>
-							<input
-								type="text"
-								value={lastName}
-								name="lastName"
-								placeholder="Enter your name"
-								onChange={(e) => onChange(e)}
-								required={true}
-							/>
-						</div>
-						<div className={style.input_col}>
-							<label>Address</label>
-							<input
-								type="text"
-								value={address1}
-								name="address1"
-								placeholder="543 N Washington St."
-								onChange={(e) => onChange(e)}
-								required={true}
-							/>
-						</div>
-					</div>
-					<div className={style.col}>
-						<div className={style.input_col}>
-							<label>State</label>
-							<select name="province" value={province} onChange={(e) => onChange(e)} required={true}>
-								<option>Select your State</option>
-								{states.map((state: any, i: number) => {
-									return (
-										<option value={state.name} key={i}>
-											{state.name}
-										</option>
-									);
-								})}
-							</select>
-						</div>
-						<div className={style.input_col}>
-							<label>City</label>
-							<input
-								type="text"
-								value={city}
-								name="city"
-								placeholder="Enter your city"
-								onChange={(e) => onChange(e)}
-								required={true}
-							/>
-						</div>
-						<div className={style.input_col}>
-							<label>Zip</label>
-							<input
-								type="text"
-								value={zip}
-								name="zip"
-								placeholder="Enter your Zip"
-								onChange={(e) => onChange(e)}
-								required={true}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className={style.btn_col}>
-					{!processing ? (
-						<button onSubmit={(e) => formSubmit(e)}>Save Shipping Info</button>
-					) : (
-						<>
-							Saving <LoadingSpinner updateStyle={{ size: '1rem', color: '#111' }} />
-						</>
-					)}
-				</div>
-			</form>
-		</div>
-	);
+  return (
+    <div className={style.form_container}>
+      <div className={style.form_header}>
+        <h2>Shipping Address Form</h2>
+        <p>Currently, only shipping within the US</p>
+        {shippingErrors.length > 0
+          ? alerts.map((alert, i) => (
+              <StoreAlert status={alert.msg} key={i} type={alert.alertType} />
+            ))
+          : null}
+      </div>
+      <form className={style.form} onSubmit={(e) => formSubmit(e)}>
+        <div className={style.grid}>
+          <div className={style.col}>
+            <div className={style.input_col}>
+              <label>First Name</label>
+              <input
+                type="text"
+                value={firstName}
+                name="firstName"
+                placeholder="Enter your name"
+                onChange={(e) => onChange(e)}
+                required={true}
+              />
+            </div>
+            <div className={style.input_col}>
+              <label>Last Name</label>
+              <input
+                type="text"
+                value={lastName}
+                name="lastName"
+                placeholder="Enter your name"
+                onChange={(e) => onChange(e)}
+                required={true}
+              />
+            </div>
+            <div className={style.input_col}>
+              <label>Address</label>
+              <input
+                type="text"
+                value={address1}
+                name="address1"
+                placeholder="543 N Washington St."
+                onChange={(e) => onChange(e)}
+                required={true}
+              />
+            </div>
+          </div>
+          <div className={style.col}>
+            <div className={style.input_col}>
+              <label>State</label>
+              <select
+                name="province"
+                value={province}
+                onChange={(e) => onChange(e)}
+                required={true}
+              >
+                <option>Select your State</option>
+                {states.map((state: any, i: number) => {
+                  return (
+                    <option value={state.name} key={i}>
+                      {state.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className={style.input_col}>
+              <label>City</label>
+              <input
+                type="text"
+                value={city}
+                name="city"
+                placeholder="Enter your city"
+                onChange={(e) => onChange(e)}
+                required={true}
+              />
+            </div>
+            <div className={style.input_col}>
+              <label>Zip</label>
+              <input
+                type="text"
+                value={zip}
+                name="zip"
+                placeholder="Enter your Zip"
+                onChange={(e) => onChange(e)}
+                required={true}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={style.btn_col}>
+          {!processing ? (
+            <button onSubmit={(e) => formSubmit(e)}>Save Shipping Info</button>
+          ) : (
+            <>
+              Saving{" "}
+              <LoadingSpinner updateStyle={{ size: "1rem", color: "#111" }} />
+            </>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 };
 
 const mapStateToProps = (state: any) => ({
-	store: state.store,
-	alerts: state.alerts,
+  store: state.store,
+  alerts: state.alerts,
 });
 
-export default connect(mapStateToProps, { submitShippingInfo })(ShippingAddressForm);
+export default connect(mapStateToProps, { submitShippingInfo })(
+  ShippingAddressForm
+);
--- components/store/checkout/UpdateInfoToggle.tsx
+++ components/store/checkout/UpdateInfoToggle.tsx
@@ -1,32 +1,40 @@
-import React from 'react';
-import style from './UpdateToggle.module.scss';
-import { connect } from 'react-redux';
-import { toggleShippingModule } from '../../../actions/store';
-import StoreAlert from '../alerts/StoreAlert';
+import React from "react";
+import style from "./UpdateToggle.module.scss";
+import { connect } from "react-redux";
+import { toggleShippingModule } from "../../../redux/actions/store";
+import StoreAlert from "../alerts/StoreAlert";
 
 interface Props {
-	store?: any;
-	toggleShippingModule: (val: boolean) => any;
-	alerts: Array<any>;
+  store?: any;
+  toggleShippingModule: (val: boolean) => any;
+  alerts: Array<any>;
 }
 
-const UpdateInfoToggle = ({ store: { shippingSaved }, toggleShippingModule, alerts }: Props) => {
-	return (
-		<div className={style.saved_info}>
-			{alerts.length > 0
-				? alerts.map((alert: any, i: number) => (
-						<StoreAlert type={alert.alertType} status={alert.msg} key={i} />
-				  ))
-				: null}
-			<h1>Shipping info was saved!</h1>
-			<button onClick={(e) => toggleShippingModule(!shippingSaved)}>Update Info</button>
-		</div>
-	);
+const UpdateInfoToggle = ({
+  store: { shippingSaved },
+  toggleShippingModule,
+  alerts,
+}: Props) => {
+  return (
+    <div className={style.saved_info}>
+      {alerts.length > 0
+        ? alerts.map((alert: any, i: number) => (
+            <StoreAlert type={alert.alertType} status={alert.msg} key={i} />
+          ))
+        : null}
+      <h1>Shipping info was saved!</h1>
+      <button onClick={(e) => toggleShippingModule(!shippingSaved)}>
+        Update Info
+      </button>
+    </div>
+  );
 };
 
 const mapStateToProps = (state: any) => ({
-	store: state.store,
-	alerts: state.alerts,
+  store: state.store,
+  alerts: state.alerts,
 });
 
-export default connect(mapStateToProps, { toggleShippingModule })(UpdateInfoToggle);
+export default connect(mapStateToProps, { toggleShippingModule })(
+  UpdateInfoToggle
+);
