0d2a8fc800aced518d9d1b176a24180a23c6766d
Alex <alexroth96@gmail.com>
2021-07-13 09:50:39 -0400

    idk

--- components/nav/navLinks.tsx
+++ components/nav/navLinks.tsx
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const navLinks = [
-  { path: "/main", name: "home", id: uuidv4() },
+  { path: "/Main", name: "home", id: uuidv4() },
   { path: "/music", name: "music", id: uuidv4() },
   { path: "/videos", name: "videos", id: uuidv4() },
   { path: "/single", name: "devils", id: uuidv4() },
-  { path: "/songbook", name: "songbook", id: uuidv4() },
+  { path: "/Songbook", name: "songbook", id: uuidv4() },
   { path: "/merch", name: "merch", id: uuidv4() },
   { path: "/about", name: "about", id: uuidv4() },
 ];
