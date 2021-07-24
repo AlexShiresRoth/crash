708b1ff0bf9a0b77a7c6e03d5e0a8bd45e814875
Alex Rothenberg <alexroth96@gmail.com>
2021-07-24 14:47:35 -0400

    dfasdfsdf

--- components/nav/navLinks.tsx
+++ components/nav/navLinks.tsx
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const navLinks = [
-  // { path: "/Main", name: "home", id: uuidv4() },
+  { path: "/Main", name: "home", id: uuidv4() },
   { path: "/music", name: "music", id: uuidv4() },
-  // { path: "/Videos", name: "videos", id: uuidv4() },
+  { path: "/Videos", name: "videos", id: uuidv4() },
   { path: "/Devils", name: "devils", id: uuidv4() },
   { path: "/SongBook", name: "songbook", id: uuidv4() },
   { path: "/merch", name: "merch", id: uuidv4() },
-  // { path: "/About", name: "about", id: uuidv4() },
+  { path: "/About", name: "about", id: uuidv4() },
 ];
