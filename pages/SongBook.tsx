9c1cd4f582ae9ee7919ad8bfb71fb84b4abc864f
Alex <alexroth96@gmail.com>
2021-07-28 13:57:11 -0400

    add tracking twitter

--- pages/SongBook.tsx
+++ pages/SongBook.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import Layout from "../UI/layout/Layout";
 import BookComponent from "../components/songbook/BookComponent";
 import ReactGA from "react-ga";
+import Head from "next/head";
 
 ReactGA.initialize("UA-172813905-2");
 
@@ -15,9 +16,61 @@ const Songbook = () => {
     }
   }, []);
   return (
-    <Layout>
-      <BookComponent />
-    </Layout>
+    <>
+      <Head>
+        {" "}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='//static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+// Insert Twitter Pixel ID and Standard Event data below
+twq('init','o6g71');
+twq('track','PageView');`,
+          }}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!(function (f, b, e, v, n, t, s) {
+				if (f.fbq) return;
+				n = f.fbq = function () {
+					n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+				};
+				if (!f._fbq) f._fbq = n;
+				n.push = n;
+				n.loaded = !0;
+				n.version = '2.0';
+				n.queue = [];
+				t = b.createElement(e);
+				t.async = !0;
+				t.src = v;
+				s = b.getElementsByTagName(e)[0];
+				s.parentNode.insertBefore(t, s);
+			})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+			fbq('init', '682418259052811');
+			fbq('track', 'PageView');`,
+          }}
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KTX0ZNDXRT"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `	window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+
+			gtag('config', 'G-KTX0ZNDXRT');`,
+          }}
+        ></script>
+      </Head>
+      <Layout>
+        <BookComponent />
+      </Layout>
+    </>
   );
 };
 
